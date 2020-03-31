0a8e35611e35ec7d14dc51a08033ca00d493dbdb
Adriann521 <adriann521@gmail.com>
2020-03-31 00:21:29 -0400

    commit

---
+++ build/precache-manifest.ab6fa77dd7d92cf0412b25b5d5fee8e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5e2074da999fbd3f2ff3eeb16a249d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3feb694d49509c7d871d",
+    "url": "/static/css/2.a33d7f97.chunk.css"
+  },
+  {
+    "revision": "209b5474b1b9e1603142",
+    "url": "/static/css/main.c23f2506.chunk.css"
+  },
+  {
+    "revision": "3feb694d49509c7d871d",
+    "url": "/static/js/2.a46b78cd.chunk.js"
+  },
+  {
+    "revision": "ac829f65dba0ed606fb00c17e2b208fa",
+    "url": "/static/js/2.a46b78cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "209b5474b1b9e1603142",
+    "url": "/static/js/main.1c349822.chunk.js"
+  },
+  {
+    "revision": "78c1f3380aba15bc3462",
+    "url": "/static/js/runtime-main.17f9c848.js"
+  }
+]);
