c092cd98d941c3f85d5cc26086c1c32071cf252d
Adriann521 <adriann521@gmail.com>
2020-04-20 20:05:25 -0400

    MOVIES ARE IN*

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6f15d53e5a340de2e5e74740cbfefd4c.js"
+  "/precache-manifest.b5a193b4ad17f272eaf49965d1bb1c34.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
